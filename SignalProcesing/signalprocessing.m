622c1e57bd6b11ac686cdfdcef6810fa7c28efbd
zhankh <zhanshi0813@gmail.com>
2020-10-21 16:42:25 -0700

    2

--- SignalProcesing/signalprocessing.m
+++ SignalProcesing/signalprocessing.m
@@ -1,20 +1,28 @@
 
 % Step 1 : Play Chirp Signal
-yi = chirp1(5000);
+%  yi = chirp1(5000);
 
 % Step 2 : Read Audio Files
-% [yo10KhzInear,Fs] = audioread("C:\Users\zhans\Desktop\Auidio\10KHz\10KHzInEar.wav");
-% [yo10KhzNotInEar,Fs] = audioread("C:\Users\zhans\Desktop\Auidio\10KHz\10KHzNotInEar.wav");
+ signal1path = "C:\Users\zhans\Desktop\Auidio\10KHz\190317_0085.wav";
+ [yo10KhzInear,Fs] = audioread(signal1path);
+ signal2path = "C:\Users\zhans\Desktop\Auidio\10KHz\10KHzInEar.wav";
+ [yo10KhzInearnowater,Fs] = audioread(signal2path);
+  signal2path = "C:\Users\zhans\Desktop\Auidio\10KHz\10KHzNotInEar.wav";
+ [yo10KhzNotInEar,Fs] = audioread(signal2path);
 
 % Step 3 : To Align two signals
- [yof, yif] = delay(yo10KhzInear(:,2), yi);
+[yof, yif] = delay(yo10KhzInear(:,2), yo10KhzNotInEar(:,2));
+diff = yof - yif;
+ plotfr(signal1path);
+
 
 % [output4500B, e] = delay(yo10KhzNotInEar(:,2), yi);
 
 % Step 4 : To use xcorr to Analyze
 % xorr 的部分
-% [c,lags] = xcorr(yof,yif);
+% [c,lags] = xcorr(yi,yo10KhzInear_2);
 % stem(lags,c)
+
 
 % CORR 的部分
 % w = 10;                               % w是滑动窗口的大小3
@@ -47,6 +55,30 @@ yi = chirp1(5000);
 % % plot(y)
 % end
 
+function[] = plotfr(aa)
+string1 = aa; %你的文件名
+[x1,Fs1] = audioread(string1); %matlab自带音频采集函数
+y1 = x1(:,2);
+subplot(221);
+plot(0:1:length(y1)-1,y1);
+title('Original signal'); %显示你采集的声音信号
+
+%下面是DFT
+N=length(y1);
+k=-N/2:N/2-1;
+w=2*pi/N*k;
+freq=w*Fs1/2/pi;
+y1_fft=fft(y1,N);%快速傅里叶变换
+subplot(212);
+plot(freq,abs(fftshift(y1_fft)));
+title('Frequency Response');
+xlabel('Frequency');
+ylabel('DB');
+axis([0 10000 0 5000]);%这个用上可以显示你想要的范围，看情况。
+
+
+end
+
 function [correctsong] = chirp1(f)
 fs = 44100;
 tv = 0.1/(f*2)
@@ -54,7 +86,7 @@ t = 0:tv:2;              % 2 secs @ 10kHz sample rate
 y = 0.1*chirp(t,0,1,f);       % Start @ DC, cross 1500Hz at t=1 sec
 z = y*0;
  specgram(y,256,1e6,256,250) % Display the spectrogram
-song = [y z y z];
+song = [y];
 correctsong = transpose(song);
 sound (song, fs)
 %plot(y)
